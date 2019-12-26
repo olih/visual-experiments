b9e94e7dc040bd9c05e3c2ca0b35139bc514267f
olih <olih@users.noreply.github.com>
2019-12-26 18:15:38 +0000

    Adds check for unique names

--- create-series.py
+++ create-series.py
@@ -4,11 +4,14 @@ import os
 from hashids import Hashids
 import json
 from datetime import date
+import argparse
+
 today = date.today()
 
-hashids = Hashids(salt="Every artist was first an amateur", min_length=8)
+hashids = Hashids(salt="Every artist was first an amateur", min_length=9)
 baseURL = "https://m.olivierhuin.com/i/"
 authorURL = "https://m.olivierhuin.com/profile"
+licenseURL = "https://creativecommons.org/licenses/by-sa/4.0"
 
 def createId():
     with open('counter.txt', 'r') as file:
@@ -18,41 +21,103 @@ def createId():
             wfile.write(str(counter))
             return hashids.encode(counter)
 
-slug = input('What is slug of the series ?')
-title = input('What is the title of the work ?')
-description = input('What is the description ?')
-keywords = input('Some keywords for the image ?')
+def splitAndTrim(separator: str, value: str):
+    results = map(lambda s: s.strip(), value.split(separator))
+    return list(results) if results else []
 
-baseName = slug.lower() + "-" + createId()
-os.mkdir("creations/" + baseName)
+def mergeKeywords(keywords1: str, keywords2: str):
+    k1 = set(splitAndTrim(",", keywords1))
+    k2 = set(splitAndTrim(",", keywords2))
+    return ",".join(list(k1|k2))
 
-mediaJson = {
-    "count": 1,
-    "baseName": baseName,
-    "keywords": keywords,
-    "items": [
-        {
-            "contentUrl": "{}{}.1.64x64.png".format(baseURL, baseName),
-            "name": "{}.1.64x64.png".format(baseName),
-            "@type": "ImageObject",
+def createBase(slug: str, keywords: str):
+    return {
+        "baseName": slug.lower() + "-" + createId(),
+        "keywords": mergeKeywords(keywords, ""),
+        "baseURL": baseURL,
+        "author": authorURL,
+        "license": licenseURL,
+        "creativeWorkStatus": "Draft",
+        "colors": "#000000 #ffffff",
+        "@type": "ImageObject",
+        "encodingFormat": "image/png"
+    }
+
+def createName(base, index: int, width: int, height: int):
+    return "{}-{}.{}x{}.png".format(base['baseName'], index, width, height)
+
+def createMediaItem(base, index: int, width: int, height: int, title: str, description: str, keywords: str ):
+    return {
+            "contentUrl": baseURL + createName(base, index, width, height),
+            "name": createName(base, index, width, height),
+            "@type": base["@type"],
             "headline": title,
             "description": description,
-            "keywords": keywords,
-            "encodingFormat": "image/png",
-            "width": "64 px",
-            "height": "64 px",
-            "colors": "#000000 #ffffff",
-            "author": authorURL,
+            "abstract": "",
+            "keywords": mergeKeywords(base['keywords'], keywords),
+            "encodingFormat": base['encodingFormat'],
+            "width": "{} px".format(width),
+            "height": "{} px".format(height),
+            "colors": base['colors'],
+            "author": base['author'],
             "datePublished": today.strftime("%Y-%m-%d"),
             "copyrightYear": today.strftime("%Y"),
-            "creativeWorkStatus": "Draft",
-            "license": "https://creativecommons.org/licenses/by-sa/4.0",
+            "creativeWorkStatus": base['creativeWorkStatus'],
+            "license": base['license']
         }
-    ]
-}
 
-def saveMediaAsJson(rows):
-    with open('creations/{}/media.json'.format(baseName), 'w') as outfile:
-        json.dump(rows, outfile, indent=2)
+def isNameUnique(jsonContent):
+    names = [item['name'] for item in jsonContent['items']]
+    return len(names) == len(set(names))
 
-saveMediaAsJson(mediaJson)
+def saveMediaAsJson(baseName: str, jsonContent):
+    if isNameUnique(jsonContent):
+        with open('creations/{}/media.json'.format(baseName), 'w') as outfile:
+            json.dump(jsonContent, outfile, indent=2)
+    else:
+            print("This unique name is already taken")
+
+def loadMediaAsJson(baseName: str):
+    with open('creations/{}/media.json'.format(baseName), 'r') as jsonfile:
+        return json.load(jsonfile)
+    
+
+parser = argparse.ArgumentParser(description = 'Create or update a new visual experiment')
+parser.add_argument("-S", "--slug", help="A slug for the folder name")
+parser.add_argument("-B", "--baseName", help="The base name for the experiment")
+parser.add_argument("-I", "--index", help="The index for the experiment", type=int)
+parser.add_argument("-K", "--keywords", help="A list of keywords separated by coma", default="")
+parser.add_argument("-T", "--title", help="A title for the image", default="")
+parser.add_argument("-D", "--description", help="A description for the image", default="")
+parser.add_argument("-W", "--width", help="The width of the image", type=int, default = 128)
+parser.add_argument("-H", "--height", help="The height of the image", type=int, default = 128)
+args= parser.parse_args()
+
+if args.slug:
+    base = createBase(slug=args.slug, keywords = args.keywords)
+    item = createMediaItem(base, index=1, width = args.width, height = args.height, title = args.title, description = args.description, keywords= args.keywords )
+    initJson = {
+        "count": 1,
+        "base": base,
+        "items": [item]
+    }
+    os.mkdir("creations/" + base['baseName'])
+    print("Creating "+ base['baseName'])
+    saveMediaAsJson(base['baseName'], initJson)
+
+elif args.baseName:
+    old = loadMediaAsJson(args.baseName)
+    base = old['base']
+    index = args.index if args.index else old['count'] + 1
+    print("Creating {} with index {}".format(base['baseName'], index))
+    item = createMediaItem(base, index=index, width = args.width, height = args.height, title = args.title, description = args.description, keywords= args.keywords)
+    newJson = {
+        "count": old['count'] if args.index else old['count'] + 1,
+        "base": base,
+        "items": old['items'] + [item]
+    }
+    saveMediaAsJson(base['baseName'], newJson)
+else:
+    print("You need to specify either slug or baseName")
+
+
