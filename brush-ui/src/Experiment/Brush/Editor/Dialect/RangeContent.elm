7fea7b27398f4c8c613401d99719e06155747016
Olivier Huin <olih@users.noreply.github.com>
2020-03-31 09:22:02 +0100

    Adds view for RangeContent

--- brush-ui/src/Experiment/Brush/Editor/Dialect/RangeContent.elm
+++ brush-ui/src/Experiment/Brush/Editor/Dialect/RangeContent.elm
@@ -1,11 +1,11 @@
-module Experiment.Brush.Editor.Dialect.RangeContent exposing (RangeContent, fromSection)
+module Experiment.Brush.Editor.Dialect.RangeContent exposing (RangeContent, fromSection, view)
 
 import Experiment.Brush.Editor.Dialect.Content as Content exposing (Content)
 import Experiment.Brush.Editor.Dialect.Section exposing (Section)
 import Experiment.Brush.Editor.Dialect.RangeParam as RangeParam exposing (RangeParam)
-import Html exposing (Html, figure, img, div)
-import Html.Attributes as Attr exposing (src)
-
+import Html exposing (Html, div)
+import Html.Attributes as Attr
+import Experiment.Brush.Editor.AppEvent exposing (Msg(..))
 
 type alias RangeContent =
     { 
@@ -17,6 +17,8 @@ fromSection: Section -> RangeContent
 fromSection section =
     RangeContent section (RangeParam.fromStringList section.lines |> Content.toContent)
 
-view : RangeContent -> Html a
-view model =
-    div [] []
+view : RangeContent -> Html Msg
+view rgContent =
+    rgContent.ranges.values
+    |> List.map RangeParam.view 
+    |> div [Attr.class ""]
