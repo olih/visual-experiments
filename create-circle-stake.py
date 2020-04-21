3176e7156b6a25620368de1d997d7253bb5e0b45
Olivier Huin <olih@users.noreply.github.com>
2020-04-21 21:16:24 +0100

    generates fractions from circle

---
+++ create-circle-stake.py
@@ -0,0 +1,47 @@
+import os
+import sys
+import argparse
+from fractions import Fraction
+from math import pi, radians, cos, sin
+
+if not (sys.version_info.major == 3 and sys.version_info.minor >= 5):
+    print("This script requires Python 3.5 or higher!")
+    print("You are using Python {}.{}.".format(sys.version_info.major, sys.version_info.minor))
+    sys.exit(1)
+
+parser = argparse.ArgumentParser(description = 'Create circle stake')
+parser.add_argument("-r", "--radius", help="the radius of circle (Fraction) first last inc next", default="1/4 1/8 -1/16 1/1")
+parser.add_argument("-a", "--angle", help="the angle of circle (Fraction) last inc next", default="1/1 1/16 1/1")
+parser.add_argument("-w", "--width", help="the width in pixel", default=None)
+args = parser.parse_args()
+
+radiusFirst, radiusLast, radiusInc, radiusNext = [Fraction(v) for v in args.radius.split(" ")]
+angleLast, angleInc, angleNext = [Fraction(v) for v in args.angle.split(" ")]
+
+
+def cosFract(fract):
+    numerator = int(1000*cos(radians(360*fract)))
+    return Fraction("{}/1000".format(numerator))
+
+def sinFract(fract):
+    numerator = int(1000*sin(radians(360*fract)))
+    return Fraction("{}/1000".format(numerator))
+
+def createFractions():
+    results = []
+    currentRadius = radiusFirst
+    currentRadiusInc = radiusInc*radiusNext
+    currentAngle = Fraction(0)
+    currentAngleInc = angleInc*angleNext
+    while currentRadius >= radiusLast and currentAngle <= angleLast:
+        x = currentRadius*cosFract(currentAngle)
+        y = currentRadius*sinFract(currentAngle)
+        results.append("{} {}".format(x, y))
+        currentRadiusInc =   currentRadiusInc*radiusNext
+        currentAngleInc = currentAngleInc*angleNext
+        currentRadius = currentRadius + currentRadiusInc
+        currentAngle = currentAngle + currentAngleInc
+    return results
+
+print(createFractions())
+
