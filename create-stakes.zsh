246a2d5ace7cdc11186a7298fd51b2773e2482e8
Olivier Huin <olih@users.noreply.github.com>
2020-04-22 13:34:13 +0100

    spirale

--- create-stakes.zsh
+++ create-stakes.zsh
@@ -3,5 +3,7 @@
 #for angle in 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21; do python create-circle-stake.py --radius "1/4 1/4 0/1 1/1" --angle "1/1 1/$angle 1/1" --width 10; done
 
 # With golden ratio
-python create-circle-stake.py --radius "1/4 1/4 0/1 1/1" --angle "1/1 1/360 1597/987"
-python create-circle-stake.py --radius "1/4 1/4 0/1 1/1" --angle "99/100 1/1 987/1597"
+# python create-circle-stake.py --radius "1/4 1/4 0/1 1/1" --angle "1/1 1/360 1597/987"
+# python create-circle-stake.py --radius "1/4 1/4 0/1 1/1" --angle "99/100 1/1 987/1597"
+
+python create-circle-stake.py --radius "1/4 1/12 -1/120 1/1" --angle "1/1 1/20 1/1"
