f7dbe65b059b37da60566f2babaafb661f073dc1
Olivier Huin <olih@users.noreply.github.com>
2020-04-23 15:31:39 +0100

    Adds sine wave same amplitude

--- create-stakes.zsh
+++ create-stakes.zsh
@@ -1,13 +1,17 @@
 #!/bin/zsh
 VIEW="cartesian"
 
-python create-sine-wave-stake.py --amplitude "1/4 1/4 0/1 1/1" --period "5/1 1/30 1/1" --view "$VIEW"
+#sine wave
+for period in 3 5 7 11
+do
+    python create-sine-wave-stake.py --amplitude "1/4 1/4 0/1 1/1" --period "$period/1 1/20 1/1" --view "$VIEW"
+done
 
 exit 0
 
 #Circles
 for angle in 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21
-do python create-circle-stake.py --radius "1/4 1/4 0/1 1/1" --angle "1/1 1/$angle 1/1" --width 10 --view "$VIEW"
+    do python create-circle-stake.py --radius "1/4 1/4 0/1 1/1" --angle "1/1 1/$angle 1/1" --width 10 --view "$VIEW"
 done
 
 # With golden ratio
