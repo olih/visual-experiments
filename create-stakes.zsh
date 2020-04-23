287b74621d60bf8cb6d28ced24dbf6c3e016f6f0
Olivier Huin <olih@users.noreply.github.com>
2020-04-23 21:40:55 +0100

    Adds ellipse for spirales

--- create-stakes.zsh
+++ create-stakes.zsh
@@ -1,6 +1,18 @@
 #!/bin/zsh
 VIEW="cartesian"
 
+# Linear spirales
+for ellipse in 1 2 3 4
+do
+for radius in 100 300 500 700
+do
+    for angle in 7 9 12 15 31
+    do  python create-circle-stake.py --radius "1/4 1/52 -1/$radius 1/1" --angle "$angle/1 1/30 1/1" --width 10 --ellipse "1/$ellipse" --view "$VIEW"
+done
+done
+done
+
+exit 0
 #Circles
 for ellipse in 1 2 3 5 7 9
 do
@@ -8,8 +20,6 @@ for angle in 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 27 33 36
     do python create-circle-stake.py --radius "1/4 1/4 0/1 1/1" --angle "1/1 1/$angle 1/1" --width 10 --ellipse "1/$ellipse" --view "$VIEW"
 done
 done
-
-exit 0
 
 #sine decrease amplitude
 python create-sine-wave-stake.py --amplitude "1/4 1/16 -1/1400 1/1" --period "7/1 1/30 1/1" --view "$VIEW"
@@ -21,19 +31,7 @@ do
     python create-sine-wave-stake.py --amplitude "1/4 1/4 0/1 1/1" --period "$period/1 1/30 1/1" --view "$VIEW"
 done
 
-#Circles
-for angle in 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21
-    do python create-circle-stake.py --radius "1/4 1/4 0/1 1/1" --angle "1/1 1/$angle 1/1" --width 10 --view "$VIEW"
-done
-
 # With golden ratio
 python create-circle-stake.py --radius "1/4 1/4 0/1 1/1" --angle "1/1 1/360 1597/987" --view "$VIEW"
 python create-circle-stake.py --radius "1/4 1/4 0/1 1/1" --angle "99/100 1/1 987/1597" --view "$VIEW"
 
-# Linear spirales
-for radius in 100 300 500 700
-do
-    for angle in 1 3 5 7 9 12 15 31
-    do  python create-circle-stake.py --radius "1/4 1/52 -1/$radius 1/1" --angle "$angle/1 1/20 1/1" --width 10 --view "$VIEW"
-done
-done
