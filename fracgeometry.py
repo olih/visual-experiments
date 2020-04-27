03408e891c6ffff618421678d6333eab2050e6af
Olivier Huin <olih@users.noreply.github.com>
2020-04-27 10:56:23 +0100

    Adds test_to_cartesian_string

--- fracgeometry.py
+++ fracgeometry.py
@@ -1,4 +1,5 @@
 from fractions import Fraction
+from typing import List
 
 class V2d:
     def __init__(self, x: Fraction, y: Fraction):
@@ -6,20 +7,23 @@ class V2d:
         self.y = y
 
     @classmethod
-    def from_string(cls, value):
+    def from_string(cls, value: str):
         x, y = value.strip().split(" ")
         return cls(Fraction(x), Fraction(y))
 
     def __str__(self):
         return "{} {}".format(self.x, self.y)
     
+    def to_cartesian_string(self, dpu: int):
+        return "({:.3f},{:.3f})".format(float(self.x*dpu), float(self.y*dpu))
+
     def __repr__(self):
         return "{} {}".format(self.x, self.y)
 
     def __add__(self, b):
         return V2d(self.x+b.x, self.y+b.y)
 
-    def __sub__(self, b):
+    def __sub__(self, b: Fraction):
         return V2d(self.x-b.x, self.y-b.y)
     
     def __mul__( self, scalar ):
@@ -28,7 +32,19 @@ class V2d:
     def square_magnitude(self):
         return self.x**2 + self.y**2
 
-        
+class V2dList:
+    
+    def __init__(self, values: List[V2d] ):
+         self.values = values
+    def __str__(self):
+        return ",".join([str(value) for value in self.values])
+    
+    def __repr__(self):
+        return ",".join([str(value) for value in self.values])
+    
+    def to_cartesian_string(self, dpu: int, sep=""):
+        return sep.join([ value.to_cartesian_string(dpu) for value in self.values])
+
 # def addPoints (la, lb):
 #     length = len(la)
 #     return [addPoint(la[i], lb[i]) for i in range(length)]
--- test_fracgeometry.py
+++ test_fracgeometry.py
@@ -1,15 +1,22 @@
 import unittest
 from fractions import Fraction
-from fracgeometry import V2d
+from fracgeometry import V2d, V2dList
 
 pt0 = V2d.from_string("0/1 0/1")
 ptA = V2d.from_string("1/4 1/3")
 ptB = V2d.from_string("1/5 1/6")
+ptC = V2d.from_string("1/7 -1/9")
+ptD = V2d.from_string("-1/13 -1/23")
+ptE = V2d.from_string("1/17 4/5")
+listABCDE = V2dList([ptA, ptB, ptC, ptD, ptE])
 
-class TestFracGeometry(unittest.TestCase):
+class TestV2d(unittest.TestCase):
 
     def test_create(self):
         self.assertEqual(str(ptA), "1/4 1/3")
+
+    def test_to_cartesian_string(self):
+        self.assertEqual(ptA.to_cartesian_string(100), "(25.000,33.333)")
 
     def test_addVector(self):
         self.assertEqual(str(pt0 + ptA), "1/4 1/3")
@@ -24,6 +31,14 @@ class TestFracGeometry(unittest.TestCase):
         self.assertEqual(str(ptA*Fraction("1/3")), "1/12 1/9")
         self.assertEqual((ptA*Fraction("2/3")).square_magnitude(), ptA.square_magnitude()*Fraction("4/9"))
 
-   
+class TestV2dList(unittest.TestCase):
+
+    def test_create(self):
+        self.assertEqual(str(listABCDE), "1/4 1/3,1/5 1/6,1/7 -1/9,-1/13 -1/23,1/17 4/5")
+    
+    def test_to_cartesian_string(self):
+        self.assertEqual(listABCDE.to_cartesian_string(100), "(25.000,33.333)(20.000,16.667)(14.286,-11.111)(-7.692,-4.348)(5.882,80.000)")
+
+
 if __name__ == '__main__':
     unittest.main()
