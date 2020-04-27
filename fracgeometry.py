521d286a1b2cb62112284c7efd42a5c326d83d73
Olivier Huin <olih@users.noreply.github.com>
2020-04-27 09:31:12 +0100

    Uses static method

--- fracgeometry.py
+++ fracgeometry.py
@@ -1,38 +1,38 @@
 from fractions import Fraction
 
-def toStr(a, b):
-    return "{} {}".format(a, b)
-
 class V2d:
-    def __init__(self, value:str):
-        self.value = value
+    def __init__(self, x: Fraction, y: Fraction):
+        self.x = x
+        self.y = y
+
+    @classmethod
+    def from_string(cls, value):
         x, y = value.strip().split(" ")
-        self.x = Fraction(x)
-        self.y = Fraction(y)
-            
+        return cls(Fraction(x), Fraction(y))
+
     def __str__(self):
-        return toStr(self.x, self.y)
+        return "{} {}".format(self.x, self.y)
     
     def __repr__(self):
-        return toStr(self.x, self.y)
+        return "{} {}".format(self.x, self.y)
 
     def __add__(self, b):
-        return V2d(toStr(self.x+b.x, self.y+b.y))
+        return V2d(self.x+b.x, self.y+b.y)
 
     def __sub__(self, b):
-        return V2d(toStr(self.x-b.x, self.y-b.y))
+        return V2d(self.x-b.x, self.y-b.y)
     
     def __mul__( self, scalar ):
-        return V2d(toStr(self.x*scalar, self.y*scalar))
+        return V2d(self.x*scalar, self.y*scalar)
 
-    def squareMagnitude(self):
+    def square_magnitude(self):
         return self.x**2 + self.y**2
 
         
-def addPoints (la, lb):
-    length = len(la)
-    return [addPoint(la[i], lb[i]) for i in range(length)]
+# def addPoints (la, lb):
+#     length = len(la)
+#     return [addPoint(la[i], lb[i]) for i in range(length)]
 
-def addWeightedPoints (la, lb, weight):
-    length = len(la)
-    return [addPoint(la[i], multiplyPoint(weight, lb[i])) for i in range(length)]
+# def addWeightedPoints (la, lb, weight):
+#     length = len(la)
+#     return [addPoint(la[i], multiplyPoint(weight, lb[i])) for i in range(length)]
--- test_fracgeometry.py
+++ test_fracgeometry.py
@@ -2,27 +2,27 @@ import unittest
 from fractions import Fraction
 from fracgeometry import V2d
 
-pt0 = "0/1 0/1"
-ptA = "1/4 1/3"
-ptB = "1/5 1/6"
+pt0 = V2d.from_string("0/1 0/1")
+ptA = V2d.from_string("1/4 1/3")
+ptB = V2d.from_string("1/5 1/6")
 
 class TestFracGeometry(unittest.TestCase):
 
     def test_create(self):
-        self.assertEqual(str(V2d(ptA)), ptA)
+        self.assertEqual(str(ptA), "1/4 1/3")
 
     def test_addVector(self):
-        self.assertEqual(str(V2d(pt0) + V2d(ptA)), ptA)
-        self.assertEqual(str(V2d(ptA) + V2d(ptB)), "9/20 1/2")
+        self.assertEqual(str(pt0 + ptA), "1/4 1/3")
+        self.assertEqual(str(ptA + ptB), "9/20 1/2")
     
     def test_subtractVector(self):
-        self.assertEqual(str(V2d(pt0) - V2d(ptA)), "-1/4 -1/3")
-        self.assertEqual(str(V2d(ptA) - V2d(pt0)), ptA)
-        self.assertEqual(str(V2d(ptA) - V2d(ptB)), "1/20 1/6")
+        self.assertEqual(str(pt0 - ptA), "-1/4 -1/3")
+        self.assertEqual(str(ptA - pt0), "1/4 1/3")
+        self.assertEqual(str(ptA - ptB), "1/20 1/6")
 
     def test_multiplyVector(self):
-        self.assertEqual(str(V2d(ptA)*Fraction("1/3")), "1/12 1/9")
-        self.assertEqual((V2d(ptA)*Fraction("2/3")).squareMagnitude(), V2d(ptA).squareMagnitude()*Fraction("4/9"))
+        self.assertEqual(str(ptA*Fraction("1/3")), "1/12 1/9")
+        self.assertEqual((ptA*Fraction("2/3")).square_magnitude(), ptA.square_magnitude()*Fraction("4/9"))
 
    
 if __name__ == '__main__':
