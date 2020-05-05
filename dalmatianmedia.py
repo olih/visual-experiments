a964f6c523b3c835d36e9395cff9d7311c676eb0
Olivier Huin <olih@users.noreply.github.com>
2020-05-05 11:17:57 +0100

    Adds DlmtTagDescription

--- dalmatianmedia.py
+++ dalmatianmedia.py
@@ -2,6 +2,7 @@ import os
 import sys
 import argparse
 from fractions import Fraction
+from typing import List, Tuple
 
 from fracgeometry import V2d, V2dList, VSegment, VPath, FractionList
 
@@ -9,6 +10,15 @@ if not (sys.version_info.major == 3 and sys.version_info.minor >= 5):
     print("This script requires Python 3.5 or higher!")
     print("You are using Python {}.{}.".format(sys.version_info.major, sys.version_info.minor))
     sys.exit(1)
+
+def stripStringArray(rawlines: str, sep=",")->List[str]:
+    return [line.strip() for line in rawlines.split(sep) if line.strip() != ""]
+
+def parseDlmtArray(line: str)-> List[str]:
+    return stripStringArray(line.replace("[", "").replace("]", ""), sep=",")
+
+def toDlmtArray(items: List[str], sep=",")->str:
+    return "[ {} ]".format(sep.join(items))
 
 # view i:1 lang en-gb xy -1/2 -1/2 width 1/1 height 1/1 -> everything
 class DlmtView:
@@ -37,3 +47,27 @@ class DlmtView:
     
     def __repr__(self):
         return "view {} lang {} xy {} width {} height {} -> {}".format(self.id, self.lang, self.xy, self.width, self.height, self.description)
+
+# tag i:1 lang en-gb same-as [ geospecies:bioclasses/P632y ] -> part of head
+class DlmtTagDescription:
+    def __init__(self, id: str, same_as: List[str] = [], lang: str = "en", description: str = "" ):
+        self.id = id
+        self.same_as = same_as
+        self.lang = lang
+        self.description = description
+    
+    @classmethod
+    def from_string(cls, line: str):
+        other, description  = line.split("->")
+        cmd, descId, langKey, langId, sameAsKey, sameAsInfo = other.split(" ", 5)
+        assert cmd == "tag", line
+        assert langKey == "lang", line
+        assert sameAsKey == "same-as", line
+        
+        return cls(id = descId, same_as= parseDlmtArray(sameAsInfo), lang = langId, description= description.strip())
+
+    def __str__(self):
+        return "tag {} lang {} same-as {} -> {}".format(self.id, self.lang, toDlmtArray(self.same_as, sep=", "), self.description)
+    
+    def __repr__(self):
+        return "tag {} lang {} same-as {} -> {}".format(self.id, self.lang, toDlmtArray(self.same_as, sep=", "), self.description)
--- test_dalmatianmedia.py
+++ test_dalmatianmedia.py
@@ -1,7 +1,7 @@
 import unittest
 from fractions import Fraction
 from fracgeometry import V2d, V2dList, VSegment, VPath, FractionList
-from dalmatianmedia import DlmtView
+from dalmatianmedia import DlmtView, DlmtTagDescription
 
 pt0 = V2d.from_string("0/1 0/1")
 ptA = V2d.from_string("1/4 1/3")
@@ -15,3 +15,9 @@ class TestDlmtView(unittest.TestCase):
     def test_convert(self):
         line = "view i:1 lang en-gb xy 1/2 -1/3 width 1 height 1/2 -> everything"
         self.assertEqual(str(DlmtView.from_string(line)), line)
+
+class TestDlmtTagDescription(unittest.TestCase):
+
+    def test_convert(self):
+        line = "tag i:1 lang en-gb same-as [ geospecies:bioclasses/P632y, geospecies:bioclasses/P631y ] -> part of head"
+        self.assertEqual(str(DlmtTagDescription.from_string(line)), line)
