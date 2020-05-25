0d8928a149792cfa252990b68ad287a7e377ce2b
Olivier Huin <olih@users.noreply.github.com>
2020-05-25 22:38:45 +0100

    crossover specimens

--- create-tortuga.py
+++ create-tortuga.py
@@ -54,6 +54,13 @@ class XpPoolConf:
         self.magnitudes: FractionList = FractionList.from_string(content["magnitudes"])
         self.rules: List[str] = content["rules"]
 
+def crossover_fractions(fractions1: str, fractions2: str)->str:
+    fl1 = FractionList.from_string(fractions1)
+    fl2 = FractionList.from_string(fractions2)
+    cut1 = len(fl1) // 2
+    cut2 = len(fl2) // 2
+    return str(FractionList(fl1.values[0:cut1] + fl2.values[cut2:]))
+
 class Experimenting:
     def __init__(self, name):
         self.name = name
@@ -145,6 +152,61 @@ class Experimenting:
                 "tags": ""
         }
 
+    def crossover_specimens(self, specimen1, specimen2):
+        # Create L-System
+        product = ProductionGame.from_crossover(specimen1["product"], specimen2["product"])
+        product.produce()
+        product_obj = product.to_obj()
+        
+        # Convert chain to brushstokes
+        tortugaconfig = TortugaConfig().set_magnitude_page_ratio(self.init.magnitude_page_ratio)
+        tortugaconfig.set_scale_magnitude_ratio_string(self.init.scale_magnitude_ratio)
+        tortugaconfig.set_brushstoke_angle_offset(self.init.brushstoke_angle_offset)
+        tortugaconfig.set_xy(self.init.xy)
+        angles = crossover_fractions(specimen1["angles"], specimen2["angles"])
+        magnitudes = crossover_fractions(specimen1["magnitudes"], specimen2["magnitudes"])
+        tortugaconfig.set_angles_string(angles)
+        tortugaconfig.set_magnitudes_string(magnitudes)
+        tortugaconfig.set_brush_ids(self.init.brushids)
+        tortugaconfig.set_chain(product.chain)
+        brushstokes = TortugaProducer(tortugaconfig).produce()
+        bstats = V2dList([bs.xy for bs in brushstokes])
+        # Create stencil aka DalmatianMedia
+        stencil = DalmatianMedia(DlmtHeaders().set_brush_page_ratio(self.init.brush_page_ratio))
+        stencil.add_view_string("view i:1 lang en xy 0 0 width 1 height 1 flags o tags all but [ ] -> everything")
+        stencil.add_tag_description_string("tag i:1 lang en same-as [] -> default tag")
+        for brush in self.init.brushes:
+            stencil.add_brush_string(brush)
+        stencil.set_brushstrokes(brushstokes)
+        allbr = stencil.page_brushstroke_list_for_view_string("view i:1 lang en xy 0 0 width 1 height 1 flags o tags all but [ ] -> everything")
+        fitbr = stencil.page_brushstroke_list_for_view_string("view i:1 lang en xy 0 0 width 1 height 1 flags O tags all but [ ] -> everything")
+        fitness = Fraction(len(fitbr), len(allbr))
+        ruleInfo = ", ".join([r["s"] + "->" + r["r"] for r in product_obj["rules"]])
+        correlation = bstats.get_correlation()
+        medianpoint = bstats.get_median_range(8)
+        if correlation > 0.9 or correlation < -0.9:
+            print("C", end="")
+            return None
+        if float(fitness) < 0.8:
+            print("F", end="")
+            return None
+        if medianpoint.x < self.init.min_median_range.x:
+            print("X", end="")
+            return None
+        if medianpoint.y < self.init.min_median_range.y:
+            print("Y", end="")
+            return None
+        summary = "Stencil based on angles [ {} ], magnitudes [ {} ] and the rules {} starting with {} resulting in {} brushstokes with a fitness of {:.2%}, correlation of {} and a median range of {}".format(angles, magnitudes, ruleInfo , product_obj["start"], len(brushstokes), float(fitness), correlation, medianpoint.to_float_string())
+        return {    
+                "id": self.incId(),  
+                "product": product_obj,
+                "angles": angles,
+                "magnitudes": magnitudes,
+                "stencil": stencil.to_obj(),
+                "summary": summary,
+                "tags": ""
+        }
+
     def createBetterSpecimen(self, attempts: int):
         specimen = self.createSpecimen()
         for _ in range(attempts):
@@ -176,6 +238,12 @@ class Experimenting:
         self.content['specimens'] = bestspecimens
             
     def createNewPopulation(self):
+        population = self.init.population
+        newspecimens = [ self.createBetterSpecimen(self.init.specimen_attempts) for _ in range(population) ]
+        validspecimens = [s for s in newspecimens if s is not None]
+        self.content['specimens'] = self.content['specimens'] + validspecimens
+    
+    def crossoverPopulation(self):
         population = self.init.population
         newspecimens = [ self.createBetterSpecimen(self.init.specimen_attempts) for _ in range(population) ]
         validspecimens = [s for s in newspecimens if s is not None]
