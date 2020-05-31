1ca32b11d5e308e0b3ed0d92375fba34ba9e8a34
Olivier Huin <olih@users.noreply.github.com>
2020-05-31 20:24:45 +0100

    Adds conversion to svg

--- dlmt-to-svg.py
+++ dlmt-to-svg.py
@@ -18,6 +18,7 @@ parser.add_argument("-f", "--format", help="Image format (svg, png)", default = 
 parser.add_argument("-p", "--prefix", help="Prefix for the generated media files", default = "")
 parser.add_argument("-W", "--width", help="The width of generated bitmap in pixels.", required = True)
 parser.add_argument("-v", "--view", help="The view to export (default, cropped, i:0...)", default = "default")
+parser.add_argument("-b", "--background", help="Background color", default = "white")
 args = parser.parse_args()
 
 dlmtfiles = glob("{}/*.dlmt".format(args.indirectory))
@@ -28,7 +29,10 @@ def read_dlmt_file(filename: str)->DalmatianMedia:
     with open(filename, 'r') as dlmtfile:
         return DalmatianMedia.from_string(dlmtfile.read())
 
-def write_svg(media: DalmatianMedia):
+def write_png(filename: str, color: str):
+    os.popen("inkscape --export-type=png --export-background '{}' {}".format(color, filename))
+
+def write_media(media: DalmatianMedia):
     filename = "{}/{}{}.svg".format(args.outdirectory,args.prefix, media.headers.get_text("name", "en"))
     if args.view == "default":
         media.to_xml_svg_file(media.create_page_pixel_coordinate_with_view(int(args.width), default_view), filename)
@@ -38,10 +42,12 @@ def write_svg(media: DalmatianMedia):
         media.to_xml_svg_file(media.create_page_pixel_coordinate_with_view(int(args.width), cropped_view), filename)
     else:
         media.to_xml_svg_file(media.create_page_pixel_coordinate(args.view, int(args.width)), filename)
+    if "png" in args.format:
+        write_png(filename, args.format)
 
 for filename in dlmtfiles:
     media = read_dlmt_file(filename)
-    write_svg(media)
+    write_media(media)
 
 finished = time()
 print("Took {} seconds thus {} second per specimen".format(finished-started, (finished-started)/len(dlmtfiles)))
